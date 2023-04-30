e576ae7950b31d57ac88e01dfce297aa3045a543
Joseph-Sailor-Usher <100800088+Joseph-Sailor-Usher@users.noreply.github.com>
2023-04-29 23:10:17 -0500

    Added pinch to sculpt

--- Assets/Scripts/MarchingCubes/HandInteractionDensityMarcher.cs
+++ Assets/Scripts/MarchingCubes/HandInteractionDensityMarcher.cs
@@ -2,42 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SimplexNoiseMarcher : MonoBehaviour
+public class HandInteractionDensityMarcher : MonoBehaviour
 {
     private MeshCollider meshCollider;
-    public float isoLevel = 0.5f, size = 1.0f, speed = 2.0f;
+    public float isoLevel = 0.5f, size = 0.1f;
     public Mesh mesh;
-    SimplexNoise noiseGenerator = new SimplexNoise();
     List<float> pointValues = new List<float>();
-    float offsetTime = 0;
     public List<Vector3> vertices = new List<Vector3>();
     public List<int> triangles = new List<int>();
     private Vector3 tempVector3;
     public int dimension = 10;
+    public DensityField densityField;
 
     void Start()
     {
         meshCollider = GetComponent<MeshCollider>();
         mesh = GetComponent<MeshFilter>().mesh;
-
         for (int i = 0; i < 8; i++)
-        {
-            pointValues.Add(noiseGenerator.noise(tbl.points[i]));
-            //Debug.Log(points[i]);
-        }
+            pointValues.Add(0.0f);
         StartCoroutine("GenerateNewField");
     }
 
     public IEnumerator GenerateNewField()
     {
-        yield return new WaitForSeconds(0.05f);
-        //noiseGenerator = new SimplexNoise();
+        yield return new WaitForSeconds(0.1f);
         vertices.Clear();
         triangles.Clear();
         mesh.Clear();
-
-
-        offsetTime += Time.deltaTime * speed;
 
         for (int xi = 0; xi < dimension - 1; xi++)
         {
@@ -50,9 +41,15 @@ public class SimplexNoiseMarcher : MonoBehaviour
                     tempVector3.z = zi;
                     for (int i = 0; i < 8; i++)
                     {
-                        pointValues[i] = noiseGenerator.noise(0.1f * (tbl.points[i] + Vector3.one * offsetTime + tempVector3));
-                        //March();
+                        //pointValues[i] = noiseGenerator.noise(0.1f * (tbl.points[i] + Vector3.one * offsetTime + tempVector3));
+                        pointValues[i] = densityField.densityField[
+                            xi + (int)tbl.points[i].x, 
+                            yi + (int)tbl.points[i].y, 
+                            zi + (int)tbl.points[i].z
+                            ];
+                        //Debug.Log(pointValues[i]);
                     }
+
                     Polygonalizer.PolygonalizeCube(isoLevel, size, transform.position + tempVector3, ref pointValues, ref vertices, ref triangles);
                 }
             }
